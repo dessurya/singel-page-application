fff39b62f18a3576ae0045ddda0d64f57eab5bfd
dessurya <fourline66@gmail.com>
2020-08-04 10:49:48 +0700

    try

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes || try 
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
